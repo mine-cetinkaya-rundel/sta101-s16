60cb3350e450a38f533560ccbfdfc744ad2af97f
mine-cetinkaya-rundel <mine@stat.duke.edu>
2016-03-27 19:46:08 -0400

    update for class data

--- Slides/05_unit5/deck1/R/veg.R
+++ Slides/05_unit5/deck1/R/veg.R
@@ -1,15 +1,16 @@
 load(url("https://stat.duke.edu/~mc301/R/fun/inference.RData"))
 
-n_veg = 8
-n_nonveg = 75
+n_veg = 20
+n_nonveg = 104 
 
 sta101 = data.frame(veg = c(rep("yes", n_veg), rep("no", n_nonveg)))
 
 inference(y = veg, data = sta101, success = "yes", 
           statistic = "proportion", type = "ht", 
           null = 0.08, alternative = "twosided", 
-          method = "simulation", seed = 10292015)
+          method = "simulation", seed = 45343424)
 
 inference(y = veg, data = sta101, success = "yes", 
           statistic = "proportion", type = "ci", 
           method = "simulation", boot_method = "se", seed = 10302015)
+
